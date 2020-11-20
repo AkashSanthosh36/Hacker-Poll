db80b359830e021f4b922b10e5f70bbe66c1732e
AkashSanthosh36 <70521282+AkashSanthosh36@users.noreply.github.com>
2020-11-20 17:50:12 +0530

    Add files via upload

---
+++ config/dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MONGO_URI: "mongodb+srv://Akash:akashsan123@cluster0-qgt1a.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    JWT_SECRET_KEY: "dsjflkdsjflkadsf"
+};
---
+++ config/prod.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MONGO_URI: process.env.MONGO_URI,
+    JWT_SECRET_KEY: process.env.JWT_SEC
+};
